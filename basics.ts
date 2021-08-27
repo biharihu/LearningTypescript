729bab4114414fd95307213ee7b42f9b8254db87
Akash Singh <53407198+biharihu@users.noreply.github.com>
2021-08-27 16:51:46 +0530

    basics typescript

---
+++ basics.ts
@@ -0,0 +1,13 @@
+let userName: string = "Akash";
+let hasLoggedIn: boolean = true;
+
+userName += " Singh";
+
+console.log(userName);
+
+let number: number = 10;
+let myRegex: RegExp = /foo/;
+
+const names: string[] = userName.split(" ");
+
+const myValues: Array<number> = [1, 2, 3, 4];
